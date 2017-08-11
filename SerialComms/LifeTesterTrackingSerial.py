b31b5d44b7b71d94fce3629032538d51378c42aa
Dave Mohamad <davidkmohamad@gmail.com>
2017-08-11 22:48:03 +0100

    plotting data from serial port

--- SerialComms/LifeTesterTrackingSerial.py
+++ SerialComms/LifeTesterTrackingSerial.py
@@ -1,7 +1,6 @@
 import serial
 import time
-# import io
-# import csv
+import matplotlib.pyplot as plt
 
 print('Python script to log data from LifeTester via serial port.\n\
 Hit ctrl + c to interrupt.')
@@ -14,6 +13,8 @@ time.sleep(1)
 serline = ''
 mylist = []
 
+plt.ion()
+
 try:
 	while ser.isOpen():
 		if ser.inWaiting():  # Or: while ser.inWaiting():
@@ -23,19 +24,25 @@ try:
 				print(serline)
 				mylist.append(serline.split(','))
 				serline = ''
+				plt.plot(
+					[mylist[i][3] for i in range(len(mylist)) if mylist[i][0] == 'scan' and mylist[i][2] == 'a'],
+					[mylist[i][4] for i in range(len(mylist)) if mylist[i][0] == 'scan' and mylist[i][2] == 'a']
+						)
+				plt.pause(0.001)
+				plt.clf()
+				plt.draw()
 			else :
-				if serchar != '\r':
+				if serchar != '\r': #ignore \r char
 					serline += serchar
 
 except KeyboardInterrupt:
-    print('interrupted!')
+	print('interrupted!')
 
 ser.close()
 ser.__del__()
 
-print(mylist)
 
 # how to print out the same element from lists within a list...
 # [[1,2,3],[4,5,6],[7,8,9]]
-# [a[i][0] for i in range((a))]
+# [a[i][0] for i in range(len(a))]
 # NB a[0] = [1,2,3]
